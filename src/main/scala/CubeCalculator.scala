454eec6be1f7b7091918912d3f4784acb51ea58b
Izaak Chater <chaterizaak@gmail.com>
2020-12-28 14:19:48 +1100

    first commit

---
+++ src/main/scala/CubeCalculator.scala
@@ -0,0 +1,9 @@
+
+  object CubeCalculator extends App {
+    def cube(x: Int) = {
+      x * x * x
+    }
+
+    def square(x:Int):Int = x*x
+  }
+
---
+++ src/test/scala/CubeCalculatorTest.scala
@@ -0,0 +1,10 @@
+import org.scalatest.FunSuite
+
+class CubeCalculatorTest extends FunSuite {
+  test("CubeCalculator.cube") {
+    assert(CubeCalculator.cube(3) === 27)
+  }
+  test("CubeCalculator.square") {
+    assert(CubeCalculator.square(9) === 81)
+  }
+}
